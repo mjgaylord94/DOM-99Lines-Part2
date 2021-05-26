58e82deba7cd6eec464668ff45f71ae94b643b27
Mitchell <Mitchell@Family-Mac-mini-2.local>
2021-05-26 10:00:25 -0500

    Created Button.

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ let friends = [`Darren`, `Rachel`, `Zoe`, `Kenzie`, `Emily Mountain`];
 // The last two lines of the song are written out seperately due to "line" being singular.
 function song(name) {
     for (let i = 99; i > 2; i--) {
-        console.log(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, ${i-1} lines of code in the file.`)
+        console.log(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, ${i - 1} lines of code in the file.`)
     };
     console.log(`2 lines of code in the file, 2 lines of code; ${name} strikes one out, clears it all out, 1 line of code in the file.`)
     console.log(`1 line of code in the file, 1 line of code; ${name} strikes one out, clears it all out, 0 lines of code in the file.`)
 };
 
-// Calling the function by looping through the friends array
-for (i = 0; i < friends.length; i++) {
-    song(friends[i])
+let button = document.createElement(`button`)
+let buttonText = document.createTextNode(`Sing!`)
+
+button.appendChild(buttonText)
+document.body.appendChild(button)
+
+button.addEventListener(`click`, buttonClicked)
+
+function buttonClicked(e) {
+
+    // Calling the function by looping through the friends array
+    for (i = 0; i < friends.length; i++) {
+        song(friends[i])
+    }
+
 }
