bf91a7d1886d092e56adf2646ddfd699027f0a36
Mitchell <Mitchell@Family-Mac-mini-2.local>
2021-05-26 11:58:19 -0500

    Assignment Complete.

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 let friends = [`Darren`, `Rachel`, `Zoe`, `Kenzie`, `Emily Mountain`];
 
-// Function to Sing the "99 lines of Code" song.  Function takes the argument of the name of the person singing.  
-// Each line of code will use String interpolation to correctly display how many lines of code there are currently (based off the index) and who is singing the song.
-// The last two lines of the song are written out seperately due to "line" being singular.
-function song(name) {
-    for (let i = 99; i > 2; i--) {
-        console.log(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, ${i - 1} lines of code in the file.`)
-    };
-    console.log(`2 lines of code in the file, 2 lines of code; ${name} strikes one out, clears it all out, 1 line of code in the file.`)
-    console.log(`1 line of code in the file, 1 line of code; ${name} strikes one out, clears it all out, 0 lines of code in the file.`)
-};
-
 let button = document.createElement(`button`)
 let buttonText = document.createTextNode(`Sing!`)
 
@@ -21,21 +10,52 @@ button.addEventListener(`click`, buttonClicked)
 
 function buttonClicked(e) {
 
-    // Calling the function by looping through the friends array
+    // Looping everything through the friends array so they get their own corresponding div
     for (i = 0; i < friends.length; i++) {
+        
+        // List of Variables each Friend gets
         let songContainer = document.createElement(`div`)
         songContainer.classList.add(`friend`)
         let containerTitle = document.createElement(`h3`)
         let containerTitleText = document.createTextNode(`${friends[i]}`)
-        let songParagraph = document.createElement(`p`)
+        let songParagraph = document.createElement(`div`)
+        songParagraph.classList.add(`song`)
 
+        // Function to Sing the "99 lines of Code" song.  Function takes the argument of the name of the person singing.  
+        // Lines 99-3 use a loop to create seperate p tags and their text
+        // The last two lines of the song are written out seperately due to "line" being singular.
+        // Each p tag is then appended to the song class div
+        function song(name) {
+            for (let i = 99; i > 2; i--) {
+                let songLine = document.createElement(`p`)
+                let songLineText = document.createTextNode(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, ${i - 1} lines of code in the file.`)
+            
+                songLine.appendChild(songLineText)
+                songParagraph.appendChild(songLine)
+            };
+
+            let lineTwo = document.createElement(`p`)
+            let lineTwoText = document.createTextNode(
+                `2 lines of code in the file, 2 lines of code; ${name} strikes one out, clears it all out, 1 line of code in the file.`)
+            lineTwo.appendChild(lineTwoText)
+            songParagraph.appendChild(lineTwo)
+
+            let lineOne = document.createElement(`p`)
+            let lineOneText = document.createTextNode(
+                `1 line of code in the file, 1 line of code; ${name} strikes one out, clears it all out, 0 lines of code in the file.`)
+            lineOne.appendChild(lineOneText)
+            songParagraph.appendChild(lineOne)
+        };
+
+        // Calling the Song Fuction for the Indexed Friend
+        song(friends[i])
+
+        // Appending each Element into one another and then to the DOM
         containerTitle.appendChild(containerTitleText)
         songContainer.appendChild(containerTitle)
         songContainer.appendChild(songParagraph)
 
         document.body.appendChild(songContainer)
-
-        song(friends[i])
     }
 
 }
